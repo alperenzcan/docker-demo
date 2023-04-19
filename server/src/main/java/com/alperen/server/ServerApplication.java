aed3e41b6cbce1c053abe7d414aa1838dd9a88ef
Alperen <80932033+alperenzcan@users.noreply.github.com>
2023-04-19 22:52:59 +0300

    compose updates

--- server/src/main/java/com/alperen/server/ServerApplication.java
+++ server/src/main/java/com/alperen/server/ServerApplication.java
@@ -2,12 +2,22 @@ package com.alperen.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/api")
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "Docker";
 	}
 
 }
--- server/src/main/java/com/alperen/server/calculation/CalculationController.java
+++ server/src/main/java/com/alperen/server/calculation/CalculationController.java
@@ -3,6 +3,7 @@ package com.alperen.server.calculation;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1.0/calculations")
+@CrossOrigin
 public class CalculationController {
 	
 	@Autowired
